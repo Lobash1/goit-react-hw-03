c893e4942c2eaffb5c5ae862ea9c36877c0f260c
Anastasiia <lobash.n@gmail.com>
2025-03-08 22:53:16 +0200

    done task 1-3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,30 @@
-import { useState } from "react";
-import UserForm from "../UserForm/UserForm";
+import ContactForm from "../ContactForm/ContactForm";
+import SearchBox from "../SearchBox/SearchBox";
 import css from "./App.module.css";
+import users from "../../contacts.json";
+import { useState } from "react";
+import ContactList from "../ContactList/ContactList";
 
 export default function App() {
-  const [users, setUsers] = useState([]);
+  const [contacts, setcontacts] = useState(users);
+  // Состояние для хранения поискового запроса
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const addUser = (newUser) => {
-    setUsers((prevUsers) => {
-      return [...prevUsers, newUser];
-    });
+  // Фильтрация контактов по имени
+  const filterContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const addContact = (newContact) => {
+    setcontacts((prevContacts) => [...prevContacts, newContact]);
   };
 
   return (
     <div className={css.container}>
-      <h1>Forms in React</h1>
-      <UserForm onAdd={addUser} />
+      <h1>Phonebook</h1>
+      <ContactForm onAddContact={addContact} />
+      <SearchBox query={searchQuery} onSearch={setSearchQuery} />
+      <ContactList contacts={filterContacts} />
     </div>
   );
 }
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,13 @@
+import Contact from "../Contact/Contact";
+import css from "./ContactList.module.css";
+
+
+export default function ContactList({ contacts = [] }) {
+  return (
+    <ul className={css.list}>
+      {contacts.map(({ id, name, number }) => (
+        <Contact key={id} name={name} number={number} />
+      ))}
+    </ul>
+  );
+}
